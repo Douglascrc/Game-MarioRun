565a442d9dfd9f5ffce3826876b8e58564cf5a64
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2023-05-14 19:45:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const loop = setInterval(()=> {
     mario.style.animation= 'none';
     mario.style.bottom= `${marioPosition}px` ; 
     
-    mario.src='/Imgs/game-over.png'; 
+    mario.src='Imgs/game-over.png'; 
     mario.style.width= '70px'; 
     mario.style.marginLeft= '50px';
 

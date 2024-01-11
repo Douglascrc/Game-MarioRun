d67355e8d638ec5cdc1ef85511d7634f57ecc2ca
Douglascrc <douglasbuscam@gmail.com>
2024-01-11 18:34:31 -0300

    add reload

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
 const mario = document.querySelector('.mario');
 const tubo = document.querySelector('.tubo');
-const placar = document.querySelector('.placar');
-const scoreElemento = document.getElementById('score');
-const recordeElemento = document.getElementById('recorde'); 
+const scoreElement = document.getElementById('score');
+const recordElement = document.getElementById('recorde');
+const gameOver = document.querySelector('.gameover');
 
-const jump = () => {
-    mario.classList.add('jump');
-    
-    setTimeout(()=> {
-        mario.classList.remove('jump');
-    },500);
-}
+const loop = setInterval(() => {
+     
+    const tuboPosition = tubo.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-const loop = setInterval(()=> {
-  
-  const tuboPosition = tubo.offsetLeft;
-  
-  
-  const marioPosition= +window.getComputedStyle(mario).bottom.replace('px','');
-  
-  if(tuboPosition <= 120  && tuboPosition > 0  && marioPosition <=80 ) {
-    tubo.style.animation = 'none';
-    tubo.style.left = `${tuboPosition}px`;
-    
-    
-    mario.style.animation= 'none';
-    mario.style.bottom= `${marioPosition}px`; 
-    
-    mario.src='Imgs/game-over.png'; 
-    mario.style.width= '70px'; 
-    mario.style.marginLeft= '50px';
+    if (tuboPosition <= 120 && tuboPosition > 0 && marioPosition <= 80) {
+      tubo.style.animation = 'none';
+      tubo.style.left = `${tuboPosition}px`;
 
-    clearInterval(loop);
-  }
-  
-},10); 
+      mario.style.animation = 'none';
+      mario.style.bottom = `${marioPosition}px`;
 
-document.addEventListener('keydown', jump);
+      mario.src = 'Imgs/game-over.png';
+      mario.style.width = '70px';
+      mario.style.marginLeft = '50px';
+
+      gameOver.style.display = 'block';
+      clearInterval(loop);
+    }
+  }, 10);
 
 let score = 0;
-let recorde = localStorage.getItem('recorde') || 0; 
-
-const atualizarPlacar = () => {
-  scoreElemento.textContent = score;
-  recordeElemento.textContent = recorde; // Atualiza o elemento do recorde na tela
-};
-
-const incrementarPontuacao = () => {
-  score++;
-  if (score > recorde) {
-    recorde = score;
-    localStorage.setItem('recorde', recorde);
-  }
-  atualizarPlacar();
-};
-
+let record = localStorage.getItem('recorde') || 0;
 
 setInterval(() => {
   const marioPositionX = mario.getBoundingClientRect().left;
 
   if (marioPositionX % 200 === 0) {
-    incrementarPontuacao();
-  }
-
+    incrementarPontuacao(); 
+  } 
 }, 100);
+
+const atualizarPlacar = () => {
+  scoreElement.textContent = score;
+  recordElement.textContent = record; 
+};
+
+const incrementarPontuacao = () => {
+  score++;
+  if (score > record) {
+    record = score;
+    localStorage.setItem('recorde', record);
+  }
+  atualizarPlacar();
+};
+
+// Event listener para reiniciar o jogo quando a tela de Game Over estiver visível
+document.addEventListener('keydown', () => {
+  if (gameOver.style.display === 'block') {
+    location.reload(true);
+  } else {
+    mario.classList.add('jump');
+    setTimeout(() => {
+      mario.classList.remove('jump');
+    }, 500);
+  }
+});
+
+
